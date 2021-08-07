4013294982decf788eab043c36afdde004193781
iemprashant <prashantgupta9876@gmail.com>
2021-08-08 01:17:31 +0530

    signup added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import login from '../Assests/login.svg';
 import styles from './Login.module.css';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import { authsliceactions } from '../redux/auth';
 import { useDispatch } from 'react-redux';
-import { useState, useRef } from 'react';
+import { useRef } from 'react';
 import { useHistory } from 'react-router';
+import { authsliceactions } from '../redux/auth';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
@@ -61,17 +61,29 @@ const LoginComp = () => {
   const history = useHistory();
   const passwordref = useRef();
   const usernameref = useRef();
-  const submithandler = () => {
+  const loginsubmithandler = () => {
     if (
       !emailref.current.value.includes('@') ||
       !passwordref.current.value.length > 3
     ) {
-      // seterror(true);
+      seterror(true);
       return;
     }
-    // seterror(false);
+    seterror(false);
     dispatch(authsliceactions.loginparticipant());
     history.push('/');
+  };
+  const signupsubmithandler = () => {
+    if (
+      !emailref.current.value.includes('@') ||
+      !passwordref.current.value.length > 3
+    ) {
+      seterror(true);
+      return;
+    }
+    seterror(false);
+    dispatch(authsliceactions.loginparticipant());
+    setValue(0);
   };
   // navss
   const classes = useStyles();
@@ -81,30 +93,38 @@ const LoginComp = () => {
     setValue(newValue);
   };
   return (
-    
+    <div>
       <div className='container'>
         <div class='row p-2 mt-5'>
           <div class='col mt-5'>
-            <img src={login} alt='' className=' ' style={{ width: '85%',height:"300px" }} />
+            <img src={login} alt='' className=' ' style={{ width: '85%', height: "300px" }} />
           </div>
           <div className='col mt-n-3'>
-            <div className='row'>
-              <h2 className={styles.bord_bottom}>Login</h2>
-            </div>
-
-            <div className={styles.error}>
-              {error && 'You must enter all fields'}
-            </div>
-            <div className={`${styles.entry} row`}>
-              <div className='col-sm-1'>
-                <MailOutlineIcon />
-              </div>
-              <div className='col-sm-11'>
-                <input
-                  ref={emailref}
-                  type='email'
-                  placeholder='Enter email id'
-                />
+            <Paper className={classes.root}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+              >
+                <Tab label="Login" {...a11yProps(0)} />
+                <Tab label="SignUp" {...a11yProps(1)} />
+              </Tabs>
+            </Paper>
+            {/* login component */}
+            <TabPanel value={value} index={0}>
+              <div className={`${styles.entry} row`}>
+                <div className='col-sm-1'>
+                  <MailOutlineIcon />
+                </div>
+                <div className='col-sm-11'>
+                  <input
+                    ref={emailref}
+                    type='email'
+                    placeholder='Enter email id'
+                  />
+                </div>
               </div>
               <div className={`${styles.entry} row`}>
                 <div className='col-sm-1'>
@@ -120,13 +140,14 @@ const LoginComp = () => {
               </div>
               <div className={`${styles.entry}  row`}>
                 <button
-                  onClick={submithandler}
+                  onClick={loginsubmithandler}
                   type='button'
                   class='btn btn-primary'
                 >
                   Login
                 </button>
               </div>
+            </TabPanel>
             {/* signup component */}
             <TabPanel value={value} index={1}>
               <div className={`${styles.entry} row`}>
@@ -167,7 +188,7 @@ const LoginComp = () => {
               </div>
               <div className={`${styles.entry}  row`}>
                 <button
-                  onClick={submithandler}
+                  onClick={signupsubmithandler}
                   type='button'
                   class='btn btn-primary'
                 >
@@ -175,10 +196,6 @@ const LoginComp = () => {
                 </button>
               </div>
             </TabPanel>
-
-            {/* <div className={styles.bot_text}>
-                            <span className="text ">DON'T HAVE AN ACCOUNT?</span> <span><Link to="/register"> Sign Up</Link></span>
-                        </div> */}
           </div>
         </div>
       </div>
