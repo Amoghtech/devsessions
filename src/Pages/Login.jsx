97ef0e76470c1b412ff6d16d6a7f2d1283f10342
iemprashant <prashantgupta9876@gmail.com>
2021-08-08 00:12:19 +0530

    updated login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,13 +2,57 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import login from '../Assests/login.svg';
 import styles from './Login.module.css';
-import MailOutlineIcon from '@material-ui/icons/MailOutline';
-import VpnKeyIcon from '@material-ui/icons/VpnKey';
-import { authsliceactions } from '../redux/auth';
 import { useDispatch } from 'react-redux';
 import { useRef } from 'react';
 import { useHistory } from 'react-router';
+import { authsliceactions } from '../redux/auth';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
 const LoginComp = () => {
   const dispatch = useDispatch();
   const emailref = useRef();
@@ -26,6 +70,13 @@ const LoginComp = () => {
     dispatch(authsliceactions.loginparticipant());
     history.push('/');
   };
+  // navss
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div>
       <div className='container'>
@@ -34,42 +85,59 @@ const LoginComp = () => {
             <img src={login} alt='' className=' ' style={{ width: '85%' }} />
           </div>
           <div className='col mt-n-3'>
-            <div className='row'>
-              <h2 className={styles.bord_bottom}>Login</h2>
-            </div>
-            <div className={`${styles.entry} row`}>
-              <div className='col-sm-1'>
-                <MailOutlineIcon />
-              </div>
-              <div className='col-sm-11'>
-                <input
-                  ref={emailref}
-                  type='email'
-                  placeholder='Enter email id'
-                />
-              </div>
-            </div>
-            <div className={`${styles.entry} row`}>
-              <div className='col-sm-1'>
-                <VpnKeyIcon />
-              </div>
-              <div className='col-sm-11'>
-                <input
-                  type='password'
-                  ref={passwordref}
-                  placeholder='Enter password'
-                />
-              </div>
-            </div>
-            <div className={`${styles.entry}  row`}>
-              <button
-                onClick={submithandler}
-                type='button'
-                class='btn btn-primary'
+            <Paper className={classes.root}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
               >
-                Login
-              </button>
-            </div>
+                <Tab label="Login" {...a11yProps(0)} />
+                <Tab label="SignUp" {...a11yProps(1)} />
+              </Tabs>
+            </Paper>
+            {/* login component */}
+            <TabPanel value={value} index={0}>
+              <div className={`${styles.entry} row`}>
+                <div className='col-sm-1'>
+                  <MailOutlineIcon />
+                </div>
+                <div className='col-sm-11'>
+                  <input
+                    ref={emailref}
+                    type='email'
+                    placeholder='Enter email id'
+                  />
+                </div>
+              </div>
+              <div className={`${styles.entry} row`}>
+                <div className='col-sm-1'>
+                  <VpnKeyIcon />
+                </div>
+                <div className='col-sm-11'>
+                  <input
+                    type='password'
+                    ref={passwordref}
+                    placeholder='Enter password'
+                  />
+                </div>
+              </div>
+              <div className={`${styles.entry}  row`}>
+                <button
+                  onClick={submithandler}
+                  type='button'
+                  class='btn btn-primary'
+                >
+                  Login
+                </button>
+              </div>
+            </TabPanel>
+            {/* signup component */}
+            <TabPanel value={value} index={1}>
+              Item Two
+            </TabPanel>
+
             {/* <div className={styles.bot_text}>
                             <span className="text ">DON'T HAVE AN ACCOUNT?</span> <span><Link to="/register"> Sign Up</Link></span>
                         </div> */}
